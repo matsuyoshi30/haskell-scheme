375bd0b6651d6995493c130a562798c934543883
matsuyoshi30 <sfbgwm30@gmail.com>
2022-02-10 21:42:39 +0900

    Return string, atom or number

--- Main.hs
+++ Main.hs
@@ -1,6 +1,14 @@
 module Main where
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+data LispVal = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -8,11 +16,34 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseString :: Parser LispVal
+parseString = do
+  _ <- char '"'
+  x <- many (noneOf "\"")
+  _ <- char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit -- many1 digit が Parser モナドを返すので、 liftM で関数 Number . read を持ち上げてモナド内の値に適用する
+
+parseExpr :: Parser LispVal
+parseExpr = parseString <|> parseAtom <|> parseNumber
+
 readExpr :: String -> String
 readExpr input =
-  case parse (spaces >> symbol) "lisp" input of
+  case parse (spaces >> parseExpr) "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value: " ++ show val
+    Right _ -> "Found value"
 
 main :: IO ()
 main = do
