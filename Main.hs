83e65b36a8707996af21481fc645998e5e92bef2
matsuyoshi30 <sfbgwm30@gmail.com>
2022-02-10 22:34:55 +0900

    Deriving show to LispVal

--- Main.hs
+++ Main.hs
@@ -9,6 +9,7 @@ data LispVal = Atom String
   | Number Integer
   | String String
   | Bool Bool
+  deriving Show
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -43,7 +44,7 @@ readExpr :: String -> String
 readExpr input =
   case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right _ -> "Found value"
+    Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do
