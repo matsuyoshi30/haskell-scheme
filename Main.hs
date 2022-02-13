4e249168ca9eb9294bf93d1d668c5e6846890d9c
matsuyoshi30 <sfbgwm30@gmail.com>
2022-02-13 17:05:30 +0900

    Fix eval

--- Main.hs
+++ Main.hs
@@ -201,6 +201,7 @@ eval :: LispVal -> LispVal
 eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
+eval val@(Atom _) = val
 eval (List [Atom "quote", val]) = val
 eval (List (Atom func : args)) = apply func $ map eval args
 
