964ce410084b886c52cff6d264585ef6b63ef758
matsuyoshi30 <sfbgwm30@gmail.com>
2022-02-18 23:25:29 +0900

    stdlib - basic numeric function

--- stdlib.scm
+++ stdlib.scm
@@ -9,3 +9,9 @@
 
 (define (curry func arg1) (lambda (arg) (apply func (cons arg1 (list arg)))))
 (define (compose f g) (lambda (arg) (f (apply g arg))))
+
+(define zero? (curry = 0))
+(define positive? (curry < 0))
+(define negative? (curry > 0))
+(define (odd? num) (= (mod num 2) 1))
+(define (even? num) (= (mod num 2) 0))
